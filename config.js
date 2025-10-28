046e3c8a8442ab535ac66798558a68da8a7ef625
SophieBijj <sophiebijjani@gmail.com>
2025-10-28 12:36:34 -0400

    Update config.js

--- config.js
+++ config.js
@@ -175,7 +175,7 @@ const SITE_CONFIG = {
 
     // Newsletter (intégrée au footer)
     newsletter: {
-        titre: "Zone de risque!",
+        titre: "DANGER",
         description: "Tu pourrais faire partie des premières personnes que j’informe des nouvelles initiatives qui germent de mon côté, et ce, jusqu’à trois fois par année. C’est risqué! "
     },
 
